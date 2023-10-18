f260127a3e7123d04d5ea9dddcc24e3ac5508567
anshadarshcse <115443905+anshadarshcse@users.noreply.github.com>
2023-10-18 14:41:32 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Changes to be committed:
    	new file:   sample.json
    	modified:   src/App.js
    	new file:   src/components/Navbar.js
    	new file:   src/components/News.js
    	new file:   src/components/NewsItem.js
    	new file:   src/components/Newsdetail.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
-  c='Ansh'
+
   render() {
     return (
       <div>
-        Hello my first class based componenet{this.c}
+        <Navbar></Navbar>
+        <News></News>
       </div>
     )
   }
---
+++ src/components/Newsdetail.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Newsdetail extends Component {
+  render() {
+    return (
+      <div>
+        Hello buddy
+        
+      </div>
+    )
+  }
+}
